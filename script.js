2bab036b7159bb9cac36073f215ccb54251412ad
stromez <zemorts@gmail.com>
2022-09-24 18:33:04 +0700

    update and make some file ofline

--- script.js
+++ script.js
@@ -1,112 +1,132 @@
-var mainContainer = document.querySelector('.main');
-var allCards = document.querySelectorAll('.card');
-// var nope = document.getElementById('nope');
-// var love = document.getElementById('love');
-
-function showcard(card, index) {
-    var newCards = document.querySelectorAll('.card:not(.removed)');
-    newCards.forEach(function (card, index) {
-        card.style.zIndex = allCards.length - index;
-        card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
-        card.style.opacity = (10 - index) / 10;
-    });
-
-    mainContainer.classList.add('loaded');
-}
 
 
-showcard();
+firebase.database().ref("Cards").once("value", function (snapshot) {
+    var mainContainer = document.querySelector('.main');
+    var allCards = document.querySelectorAll('.card');
+    // var nope = document.getElementById('nope');
+    // var love = document.getElementById('love');
 
-updatehammer(document.querySelectorAll('.card'))
-
-function updatehammer(newcard) {
-    newcard.forEach(function (el) {
-        var hammertime = new Hammer(el);
-
-        hammertime.on('pan', function (event) {
-            el.classList.add('moving');
+    var listcards = Object.values(JSON.parse(JSON.stringify(snapshot.val(), null, 3)));
+    // console.log(listcards)
+    // listcards.forEach(addcard);
+    
+    function showcard(card, index) {
+        var newCards = document.querySelectorAll('.card:not(.removed)');
+        newCards.forEach(function (card, index) {
+            card.style.zIndex = allCards.length - index;
+            card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+            card.style.opacity = (10 - index) / 10;
         });
 
-        hammertime.on('pan', function (event) {
-            if (event.deltaX === 0) return;
-            if (event.center.x === 0 && event.center.y === 0) return;
+        mainContainer.classList.add('loaded');
+    }
 
-            // mainContainer.classList.toggle('main_love', event.deltaX > 0);
-            // mainContainer.classList.toggle('main_nope', event.deltaX < 0);
 
-            var xMulti = event.deltaX * 0.03;
-            var yMulti = event.deltaY / 80;
-            var rotate = xMulti * yMulti;
+    showcard();
 
-            event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
-        });
+    updatehammer(document.querySelectorAll('.card'))
 
-        hammertime.on('panend', function (event) {
-            el.classList.remove('moving');
-            // console.log(el)
-            el.setAttribute('data-answer', event.deltaX > 0)
-            // mainContainer.classList.remove('main_love');
-            // mainContainer.classList.remove('main_nope');
+    function updatehammer(newcard) {
+        newcard.forEach(function (el) {
+            var hammertime = new Hammer(el);
 
-            var moveOutWidth = document.body.clientWidth;
-            var keep = Math.abs(event.deltaX) < 40 || Math.abs(event.velocityX) < 0.5;
+            hammertime.on('pan', function (event) {
+                el.classList.add('moving');
+            });
 
-            event.target.classList.toggle('removed', !keep);
-            if (keep) {
-                event.target.style.transform = '';
-                
-            } else {
-                var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
-                var toX = event.deltaX > 0 ? endX : -endX;
-                var endY = Math.abs(event.velocityY) * moveOutWidth;
-                var toY = event.deltaY > 0 ? endY : -endY;
+            hammertime.on('pan', function (event) {
+                if (event.deltaX === 0) return;
+                if (event.center.x === 0 && event.center.y === 0) return;
+
+                // mainContainer.classList.toggle('main_love', event.deltaX > 0);
+                // mainContainer.classList.toggle('main_nope', event.deltaX < 0);
+
                 var xMulti = event.deltaX * 0.03;
-                var yMulti = event.deltaY / 40;
+                var yMulti = event.deltaY / 80;
                 var rotate = xMulti * yMulti;
-                // ?nên cho remove hay không thì đéo biết nhé
-                event.target.remove()
-                event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
-                showcard();
 
-                // TODO send lệnh load thêm, nêu như dưới 5 card có class remove thì load thêm, không thì thôi, còn dưới 5 cái để load thì skip
-            }
+                event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+            });
+
+            hammertime.on('panend', function (event) {
+                el.classList.remove('moving');
+                // console.log(el)
+                el.setAttribute('data-answer', event.deltaX > 0)
+                // mainContainer.classList.remove('main_love');
+                // mainContainer.classList.remove('main_nope');
+
+                var moveOutWidth = document.body.clientWidth;
+                var keep = Math.abs(event.deltaX) < 40 || Math.abs(event.velocityX) < 0.5;
+
+                event.target.classList.toggle('removed', !keep);
+                if (keep) {
+                    event.target.style.transform = '';
+
+                } else {
+                    var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
+                    var toX = event.deltaX > 0 ? endX : -endX;
+                    var endY = Math.abs(event.velocityY) * moveOutWidth;
+                    var toY = event.deltaY > 0 ? endY : -endY;
+                    var xMulti = event.deltaX * 0.03;
+                    var yMulti = event.deltaY / 40;
+                    var rotate = xMulti * yMulti;
+                    // ?nên cho remove hay không thì đéo biết nhé
+                    event.target.remove()
+                    event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+                    showcard();
+
+                    // TODO send lệnh load thêm, nêu như dưới 5 card có class remove thì load thêm, không thì thôi, còn dưới 5 cái để load thì skip
+
+                    loadonemorecard()
+                }
+            });
         });
-    });
-}
+    }
 
-
-function createButtonListener(love) {
-    return function (event) {
-        console.log('skip')
-        var cards = document.querySelectorAll('.card:not(.removed)');
-        var moveOutWidth = document.body.clientWidth * 1.5;
-
-        if (!cards.length) return false;
-
-        var card = cards[0];
-
-        card.classList.add('removed');
-
-        if (love) {
-            card.style.transform = 'translate(' + moveOutWidth + 'px, -80px) rotate(-20deg)';
-        } else {
-            card.style.transform = 'translate(-' + moveOutWidth + 'px, -80px) rotate(20deg)';
+    function loadonemorecard() {
+        if ($('.card').length <= 5) {
+            var element = (Math.floor(Math.random()*listcards.length))
+            addcard(listcards[element]) 
         }
-
-        showcard();
-
-        event.preventDefault();
-    };
-}
-
-var nopeListener = createButtonListener(false);
-var loveListener = createButtonListener(true);
-
-nope.addEventListener('click', nopeListener);
-// love.addEventListener('click', loveListener);
+    }
+    
+    
 
 
-var cardtemp = `<div class="card">
+
+
+    function createButtonListener(love) {
+        return function (event) {
+            console.log('skip')
+            var cards = document.querySelectorAll('.card:not(.removed)');
+            var moveOutWidth = document.body.clientWidth * 1.5;
+
+            if (!cards.length) return false;
+
+            var card = cards[0];
+
+            card.classList.add('removed');
+
+            if (love) {
+                card.style.transform = 'translate(' + moveOutWidth + 'px, -80px) rotate(-20deg)';
+            } else {
+                card.style.transform = 'translate(-' + moveOutWidth + 'px, -80px) rotate(20deg)';
+            }
+
+            showcard();
+
+            event.preventDefault();
+        };
+    }
+
+    var nopeListener = createButtonListener(false);
+    var loveListener = createButtonListener(true);
+
+    nope.addEventListener('click', nopeListener);
+    // love.addEventListener('click', loveListener);
+
+
+    var cardtemp = `<div class="card">
 <div class="truth">   
     <div class="title">THẬT:</div>
     <div class="cauhoi">%TRUTH%</div>
@@ -121,17 +141,15 @@ var cardtemp = `<div class="card">
 </div>
 </div>`
 
-function addcard(data) {
-    var div = cardtemp;
-    var div = div.replaceAll('%TRUTH%', data.truth);
-    var div = div.replaceAll('%DARE%', data.dare);
-    $('.cards').append(div);
-    updatehammer(document.querySelectorAll('.card'))
-    showcard();
-}
+    function addcard(data) {
+        var div = cardtemp;
+        var div = div.replaceAll('%TRUTH%', data.truth);
+        var div = div.replaceAll('%DARE%', data.dare);
+        $('.cards').append(div);
+        updatehammer(document.querySelectorAll('.card'))
+        showcard();
+    }
 
-firebase.database().ref("Cards").once("value", function (snapshot) {
-    var listcards = Object.values(JSON.parse(JSON.stringify(snapshot.val(), null, 3)));
-    console.log(listcards)
-    listcards.forEach(addcard);
+
+
 })
--- script/add.js
+++ script/add.js
@@ -1,8 +1,8 @@
 function send() {
-        firebase.database().ref(`Cards`).push({
-            dare: $("#dare").val(),
-            truth: $("#truth").val(),
-        }).then(result => {        
-            console.log('done')
-        })
+    firebase.database().ref(`Cards`).push({
+        dare: $("#dare").val(),
+        truth: $("#truth").val(),
+    }).then(result => {
+        console.log('done')
+    })
 }
